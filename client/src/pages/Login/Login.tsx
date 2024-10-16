f0a30c73d662a715307d70c328e142f205708769
saim hashmi <hashmisaim037@gmail.com>
2024-10-16 13:33:46 -0400

    login page animation using framer motion

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useAccountContext } from "../../context";
 import { Base as Layout } from "@/layouts";
 import "./Login.style.scss";
+import { motion } from "framer-motion";
 
 function Login() {
   const [message, setMessage] = useState(null);
@@ -16,13 +17,41 @@ function Login() {
     }
   };
 
+  const panelVariants = {
+    hidden: { y: "100%", opacity: 0 },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        stiffness: 100,
+        damping: 20,
+      },
+    },
+  };
+
+  const staggerChildren = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
   return (
     <Layout>
       <div className="Login">
-        <div className="Login__panel">
-          <div className="Login__panel__content">
-            <img src="/carleton_logo_black.png"></img>
-            <div className="Login__panel__content__message">
+        <motion.div
+          className="Login__panel"
+          initial="hidden"
+          animate="show"
+          variants={panelVariants}
+        >
+          <motion.div className="Login__panel__content" variants={staggerChildren}>
+            <motion.img src="/carleton_logo_black.png" variants={staggerChildren}></motion.img>
+            <motion.div className="Login__panel__content__message" variants={staggerChildren}>
               <div>Welcome to the Carleton SSO Federated Portal.</div>
               <div>
                 Enter your{" "}
@@ -31,24 +60,25 @@ function Login() {
                 </a>{" "}
                 username and password.
               </div>
-            </div>
-            {message && <p>{message}</p>}
-            <div className="Login__panel__content__input">
+            </motion.div>
+            {message && <motion.p variants={staggerChildren}>{message}</motion.p>}
+            <motion.div className="Login__panel__content__input" variants={staggerChildren}>
               <input type="text" placeholder="MyCarletonOne username"></input>
               <input type="password" placeholder="Password"></input>
-            </div>
-            <div className="Login__panel__content__checkbox">
+            </motion.div>
+            <motion.div className="Login__panel__content__checkbox" variants={staggerChildren}>
               <input type="checkbox"></input>
               <label>Keep me signed in</label>
-            </div>
-            <button
+            </motion.div>
+            <motion.button
               className="Login__panel__button"
               onClick={() => attemptLogin()}
+              variants={staggerChildren}
             >
               Sign In
-            </button>
-          </div>
-        </div>
+            </motion.button>
+          </motion.div>
+        </motion.div>
       </div>
     </Layout>
   );
