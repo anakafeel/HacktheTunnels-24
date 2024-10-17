d7b840b2a6226305c18418ecbb9bef306dd8ec3e
saim hashmi <hashmisaim037@gmail.com>
2024-10-17 16:22:04 -0400

    bug fixes

--- client/src/layouts/Central/HeaderLinks/HeaderLinks.tsx
+++ client/src/layouts/Central/HeaderLinks/HeaderLinks.tsx
@@ -7,9 +7,9 @@ function HeaderLinks() {
   const { logout } = useAccountContext();
   return (
     <div className="HeaderLinks">
-      <Link to={"/"} className={buttonVariants({ variant: "outline" })}>Return To Menu</Link>|
-      <Link to={"/sitemap"} className={buttonVariants({ variant: "outline" })}>Site Map</Link>|
-      <Link to={"/help"} className={buttonVariants({ variant: "outline" })}>Help</Link>|
+      <Link to={"/"} className={buttonVariants({ variant: "outline" })}>Return To Menu</Link>
+      <Link to={"/sitemap"} className={buttonVariants({ variant: "outline" })}>Site Map</Link>
+      <Link to={"/help"} className={buttonVariants({ variant: "outline" })}>Help</Link>
       <Link to={"/login"} className={buttonVariants({ variant: "outline" })}>
         <span onClick={() => logout()}>Logout</span>
       </Link>
