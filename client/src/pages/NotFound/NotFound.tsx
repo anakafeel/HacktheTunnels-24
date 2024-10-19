b43ce6ebf2e135bf9d74839c472af57ac0e13afe
dulaaaz <hussainabdullah767@gmail.com>
2024-10-19 13:34:35 -0400

    finalized 404 page

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,12 +1,24 @@
+
+
 import { Central as Layout } from "@/layouts";
 import "./NotFound.style.scss";
 
 function NotFound() {
   return (
     <Layout title={"Page Not Found"}>
-      <h1>404</h1>
+      <div className="not-found-container">
+        <h1 className="not-found-title">404</h1>
+        <p className="not-found-message">Oops! Looks like you’ve wandered off the map.</p>
+        <div className="not-found-suggestions">
+          <p></p>
+          <button className="not-found-home-button" onClick={() => window.location.href = '/'}>
+            🧭 Go to Home
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 }
 
 export default NotFound;
+
