709599a451b1f191800280b97a1319c4d106a010
saim hashmi <hashmisaim037@gmail.com>
2024-10-19 16:51:46 -0400

    end commit

--- client/src/pages/BuildTimetable/ResultsSection/ResultsSection.tsx
+++ client/src/pages/BuildTimetable/ResultsSection/ResultsSection.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ScheduledEvent } from "@/infrastructure/ServiceAPI";
 import "./ResultsSection.style.scss";
 
@@ -7,37 +8,32 @@ interface ResultsSectionProps {
 }
 
 function ResultsSection({ scheduledEvents, addEvent }: ResultsSectionProps) {
+  const [today, setToday] = useState("");
+
+  useEffect(() => {
+    const currentDay = new Date().toLocaleString("en-US", { weekday: "long" });
+    setToday(currentDay);
+  }, []);
+
   return (
     <div className="ResultsSection no-dark-mode">
       <div className="ResultsSection__topbar no-dark-mode">
         <div className="ResultsSection__topbar__item ResultsSection__add"></div>
-        <div className="ResultsSection__topbar__item ResultsSection__crn">
-          CRN
-        </div>
-        <div className="ResultsSection__topbar__item ResultsSection__subject">
-          Subject
-        </div>
-        <div className="ResultsSection__topbar__item ResultsSection__section">
-          Section
-        </div>
-        <div className="ResultsSection__topbar__item ResultsSection__title">
-          Title
-        </div>
-        <div className="ResultsSection__topbar__item ResultsSection__credit">
-          Credits
-        </div>
-        <div className="ResultsSection__topbar__item ResultsSection__type">
-          Schedule
-        </div>
-        <div className="ResultsSection__topbar__item ResultsSection__instructor">
-          Instructor
-        </div>
+        <div className="ResultsSection__topbar__item ResultsSection__crn">CRN</div>
+        <div className="ResultsSection__topbar__item ResultsSection__subject">Subject</div>
+        <div className="ResultsSection__topbar__item ResultsSection__section">Section</div>
+        <div className="ResultsSection__topbar__item ResultsSection__title">Title</div>
+        <div className="ResultsSection__topbar__item ResultsSection__credit">Credits</div>
+        <div className="ResultsSection__topbar__item ResultsSection__type">Schedule</div>
+        <div className="ResultsSection__topbar__item ResultsSection__instructor">Instructor</div>
       </div>
       <div className="ResultsSection__results">
         {scheduledEvents.map((event, index) => (
           <div
             key={index}
             className={`ResultsSection__result ${
+              event.days.includes(today) ? "ResultsSection__result--highlight" : ""
+            } ${
               index % 2 === 0
                 ? "ResultsSection__result--gray"
                 : "ResultsSection__result--light-gray"
@@ -94,12 +90,8 @@ function ResultsSection({ scheduledEvents, addEvent }: ResultsSectionProps) {
               <div className="ResultsSection__credit no-dark-mode">
                 {event.credit}
               </div>
-              <div className="ResultsSection__type no-dark-mode">
-                {event.type}
-              </div>
-              <div className="ResultsSection__instructor no-dark-mode">
-                {event.instructor}
-              </div>
+              <div className="ResultsSection__type no-dark-mode">{event.type}</div>
+              <div className="ResultsSection__instructor no-dark-mode">{event.instructor}</div>
             </div>
             <div className="ResultsSection__result__content no-dark-mode">
               <div>
